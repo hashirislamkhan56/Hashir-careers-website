5292dace2148d72555b00d2f9c43f725a6a7a38b
Hashir Islam Khan <73400831+hashirislamkhan56@users.noreply.github.com>
2023-03-19 21:48:12 +0000

    Added Dynamic Data & API

--- main.py
+++ main.py
@@ -1,10 +1,46 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+     'id': 1,
+    'title': 'Software Engineer',
+    'location': 'Chicago, USA',
+    'salary': '$. 98,000'
+  
+  }, 
+  {
+    'id': 2,
+    'title': 'Back-End Engineer',
+    'location': 'Remote',
+    'salary':  '$. 87,000'
+  
+  },
+  {
+    'id': 3,
+    'title': 'Data Base Web Dev',
+    'location': 'Toronto, CA',
+    'salary': '$. 105,000'
+  
+  },
+  {
+    'id': 4,
+    'title': 'Data Scientist ',
+    'location': 'Montreal, CA',
+    'salary': '$. 68,000'
+  
+  }
+]
+
+
 @app.route("/")
-def hello_World():
-  return render_template("home.html")
+def hello_hashir():
+  return render_template("home.html", jobs=JOBS, company_name='Hashir')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == '__main__':
